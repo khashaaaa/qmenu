c5894711643c104f21182f153204b5544b03bbae
khashaaaa <forwardent01.com>
2021-04-01 00:51:01 +0800

    From home last

--- comps/sections/Sect3.js
+++ comps/sections/Sect3.js
@@ -9,15 +9,15 @@ const Sect3 = () => (
             <div className="left">
                 <div className="item">
                     <h3>Өрөөндөө хоол захиалах</h3>
-                    <Image src="/sect3/order.png" width={80} height={80} alt="Хоол захиалах" />
+                    <Image src="/sect3/order.png" width={60} height={60} alt="Хоол захиалах" />
                 </div>
                 <div className="item">
                     <h3>Массаж захиалах</h3>
-                    <Image src="/sect3/massage.png" width={80} height={80} alt="Массаж захиалах" />
+                    <Image src="/sect3/massage.png" width={60} height={60} alt="Массаж захиалах" />
                 </div>
                 <div className="item">
                     <h3>Өрөө цэвэрлүүлэх хүсэлт илгээх</h3>
-                    <Image src="/sect3/request.png" width={80} height={80} alt="Өрөө цэвэрлүүлэх хүсэлт илгээх" />
+                    <Image src="/sect3/request.png" width={60} height={60} alt="Өрөө цэвэрлүүлэх хүсэлт илгээх" />
                 </div>
             </div>
             <div className="wrap">
@@ -32,15 +32,15 @@ const Sect3 = () => (
             <div className="right">
                 <div className="item">
                     <h3>Хувцас угаалга захиалах</h3>
-                    <Image src="/sect3/laundry.png" width={80} height={80} alt="Хувцас угаалга захиалах" />
+                    <Image src="/sect3/laundry.png" width={60} height={60} alt="Хувцас угаалга захиалах" />
                 </div>
                 <div className="item">
                     <h3>Өрөө хүлээлгэн өгөх хүсэлт илгээх</h3>
-                    <Image src="/sect3/handover.png" width={80} height={80} alt="Өрөө хүлээлгэн өгөх хүсэлт илгээх" />
+                    <Image src="/sect3/handover.png" width={60} height={60} alt="Өрөө хүлээлгэн өгөх хүсэлт илгээх" />
                 </div>
                 <div className="item">
                     <h3>Үйлчлүүлэгчийн санал хүсэлт авах</h3>
-                    <Image src="/sect3/feedback.png" width={80} height={80} alt="Үйлчлүүлэгчийн санал хүсэлт авах" />
+                    <Image src="/sect3/feedback.png" width={60} height={60} alt="Үйлчлүүлэгчийн санал хүсэлт авах" />
                 </div>
             </div>
         </div>
--- comps/sections/Sect6.js
+++ comps/sections/Sect6.js
@@ -1,5 +1,3 @@
-import Image from 'next/image'
-
 const Sect6 = () => {
 
     return (
